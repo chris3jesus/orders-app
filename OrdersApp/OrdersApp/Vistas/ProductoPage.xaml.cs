80cf40c231255f567434bc8cdac6607d0a05fa6e
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-03-11 09:13:05 -0500

    feat: add producto page

---
+++ OrdersApp/OrdersApp/Vistas/ProductoPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using OrdersApp.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OrdersApp.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProductoPage : ContentPage
+    {
+        public ProductoPage(ProductoModel producto)
+        {
+            InitializeComponent();
+            BindingContext = producto;
+        }
+    }
+}
