5d01d3dbd6cf7d387737b6dd98697841825f6d78
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-03-11 08:40:38 -0500

    feat: add login page

--- OrdersApp/OrdersApp/MainPage.xaml.cs
+++ OrdersApp/OrdersApp/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OrdersApp.Vistas;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -13,6 +14,24 @@ namespace OrdersApp
         public MainPage()
         {
             InitializeComponent();
+
+            CodigoEntry.Text = "999";
+            ClaveEntry.Text = "prueba";
+        }
+
+        private async void OnLoginButtonClicked(object sender, EventArgs e)
+        {
+            string codigo = CodigoEntry.Text;
+            string clave = ClaveEntry.Text;
+
+            if (codigo == "999" && clave == "prueba")
+            {
+                await Navigation.PushAsync(new MenuPage(codigo));
+            }
+            else
+            {
+                await DisplayAlert("Error", "Datos incorrectos", "Aceptar");
+            }
         }
     }
 }
