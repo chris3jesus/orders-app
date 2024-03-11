8312a29d58ddef916f46afe5166212e39d6d45b0
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-03-11 08:53:55 -0500

    feat: add menu page view model

---
+++ OrdersApp/OrdersApp/ViewModels/MenuPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace OrdersApp.ViewModels
+{
+    public class MenuPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _welcomeMessage;
+
+        public string WelcomeMessage
+        {
+            get { return _welcomeMessage; }
+            set
+            {
+                if (_welcomeMessage != value)
+                {
+                    _welcomeMessage = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(WelcomeMessage)));
+                }
+            }
+        }
+
+        public MenuPageViewModel(string codigoVendedor)
+        {
+            WelcomeMessage = $"Bienvenido {codigoVendedor}";
+        }
+    }
+}
