1429508aab7351f7489f5879cedcef72c29095b1
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-03-11 09:10:56 -0500

    feat: add productos list page

--- OrdersApp/OrdersApp/Vistas/ProductosListPage.xaml.cs
+++ OrdersApp/OrdersApp/Vistas/ProductosListPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OrdersApp.Modelos;
+using OrdersApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,12 +11,25 @@ using Xamarin.Forms.Xaml;
 
 namespace OrdersApp.Vistas
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class ProductosListPage : ContentPage
-	{
-		public ProductosListPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProductosListPage : ContentPage
+    {
+        public ProductosListPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+                return;
+
+            var viewModel = BindingContext as ProductosListViewModel;
+            var selectedProducto = e.SelectedItem as ProductoModel;
+
+            viewModel?.SeleccionarProductoCommand.Execute(selectedProducto);
+
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
 }
