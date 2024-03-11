c96b9d95c43e6dd1efec99d99b555b2f4c508b63
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-03-11 08:56:02 -0500

    feat: add menu page

---
+++ OrdersApp/OrdersApp/Vistas/MenuPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using OrdersApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OrdersApp.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPage : ContentPage
+    {
+        public MenuPage(string codigoVendedor)
+        {
+            InitializeComponent();
+            BindingContext = new MenuPageViewModel(codigoVendedor);
+        }
+
+        private async void OnPedidosButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PedidosListPage());
+        }
+
+        private async void OnClientesButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ClientesListPage());
+        }
+
+        private async void OnProductosButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProductosListPage());
+        }
+    }
+}
