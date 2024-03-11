4f1c1ddbb2bab7e1e1b90b87c90633bc8c902b82
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-03-11 09:01:29 -0500

    feat: add producto model

---
+++ OrdersApp/OrdersApp/Modelos/ProductoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrdersApp.Modelos
+{
+    class ProductoModel
+    {
+        public int Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public string Marca { get; set; }
+        public string Presentacion { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+    }
+}
