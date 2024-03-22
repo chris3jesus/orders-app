e3d1d89d1fd3c539e3e6cfede0941d6c0eece2d6
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-03-22 10:40:52 -0500

    feat: add vendedor model

---
+++ OrdersApp/OrdersApp/Modelos/VendedorModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrdersApp.Modelos
+{
+    public class VendedorModel
+    {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Clave { get; set; }
+    }
+}
