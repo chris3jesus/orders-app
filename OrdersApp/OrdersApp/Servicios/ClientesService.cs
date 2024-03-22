239d83c673825a6f73c2c9714ec5639552e8f739
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-03-22 08:12:50 -0500

    feat: add clientes service

---
+++ OrdersApp/OrdersApp/Servicios/ClientesService.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using OrdersApp.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdersApp.Servicios
+{
+    public class ClientesService
+    {
+        private const string Host = "http://10.0.2.2:5077";
+        private const string Url = Host + "/api/clientes";
+
+        public async Task<List<ClienteModel>> BuscarClientes(string textoBusqueda)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var url = $"{Url}/{textoBusqueda}";
+                    var response = await client.GetAsync(url);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        return JsonConvert.DeserializeObject<List<ClienteModel>>(content);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error al obtener los clientes: " + response.StatusCode);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error en la solicitud HTTP: " + ex.Message);
+            }
+            return null;
+        }
+    }
+}
