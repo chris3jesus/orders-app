0653b5be80eb10b962a254fedbd3447eadc63e94
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-03-22 10:41:48 -0500

    feat: add login service

---
+++ OrdersApp/OrdersApp/Servicios/LoginService.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using OrdersApp.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdersApp.Servicios
+{
+    public class LoginService
+    {
+        private const string Host = "http://10.0.2.2:5077";
+        private const string Url = Host + "/api/login";
+
+        public async Task<VendedorModel> LoginAsync(string codigo, string clave)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                try
+                {
+                    var loginRequest = new { Codigo = codigo, Clave = clave };
+                    string jsonRequest = JsonConvert.SerializeObject(loginRequest);
+                    var response = await httpClient.PostAsync(Url, new StringContent(jsonRequest, Encoding.UTF8, "application/json"));
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string jsonResponse = await response.Content.ReadAsStringAsync();
+                        var vendedor = JsonConvert.DeserializeObject<VendedorModel>(jsonResponse);
+                        return vendedor;
+                    }
+                    else
+                    {
+                        throw new Exception("No se pudo iniciar sesión. Verifique sus credenciales.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+    }
+}
