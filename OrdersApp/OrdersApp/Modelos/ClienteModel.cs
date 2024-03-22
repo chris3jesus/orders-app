ec95ea94d7f3af9e991f68a42d8e77144cbcf3c8
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-03-22 08:11:20 -0500

    feat: add cliente model

---
+++ OrdersApp/OrdersApp/Modelos/ClienteModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrdersApp.Modelos
+{
+    public class ClienteModel
+    {
+        public int Codigo { get; set; }
+        public string Documento { get; set; }
+        public string Razon { get; set; }
+        public string Nombre { get; set; }
+        public string Comercial { get; set; }
+        public List<DireccionModel> Direcciones { get; set; }
+        public int Vendedor { get; set; }
+    }
+
+    public class DireccionModel
+    {
+        public int Codigo { get; set; }
+        public string Direccion { get; set; }
+        public string Distrito { get; set; }
+        public string Provincia { get; set; }
+        public string Departamento { get; set; }
+    }
+}
