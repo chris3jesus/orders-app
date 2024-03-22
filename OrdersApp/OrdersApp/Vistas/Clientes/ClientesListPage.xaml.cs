d8977b01f10444b3072465077edd1c9802525238
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-03-22 08:48:05 -0500

    feat: add clientes list page

--- OrdersApp/OrdersApp/Vistas/Clientes/ClientesListPage.xaml.cs
+++ OrdersApp/OrdersApp/Vistas/Clientes/ClientesListPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OrdersApp.Modelos;
+using OrdersApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,12 +11,21 @@ using Xamarin.Forms.Xaml;
 
 namespace OrdersApp.Vistas
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class ClientesListPage : ContentPage
-	{
-		public ClientesListPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ClientesListPage : ContentPage
+    {
+        public ClientesListPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null) return;
+            var viewModel = BindingContext as ClientesListViewModel;
+            var selectedCliente = e.SelectedItem as ClienteModel;
+            viewModel?.SeleccionarClienteCommand.Execute(selectedCliente);
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
 }
