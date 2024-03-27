c509b03fe2dfd6c75e624b8519e08706946fc2fe
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-03-27 10:00:24 -0500

    feat: add pedidos main view model

---
+++ OrdersApp/OrdersApp/ViewModels/PedidosMainPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using OrdersApp.Modelos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace OrdersApp.ViewModels
+{
+    public class PedidosMainPageViewModel : INotifyPropertyChanged
+    {
+        private VendedorModel _vendedor;
+
+        public VendedorModel Vendedor
+        {
+            get { return _vendedor; }
+            set { _vendedor = value; OnPropertyChanged(nameof(Vendedor)); }
+        }
+
+        public PedidosMainPageViewModel(VendedorModel vendedor)
+        {
+            Vendedor = vendedor;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
