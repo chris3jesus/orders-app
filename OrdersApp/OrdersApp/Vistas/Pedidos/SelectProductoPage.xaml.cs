6a6a6f45619ffa91fef36309573c478de86699e2
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-03-27 09:57:45 -0500

    feat: add select producto page

---
+++ OrdersApp/OrdersApp/Vistas/Pedidos/SelectProductoPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using OrdersApp.Modelos;
+using OrdersApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OrdersApp.Vistas.Pedidos
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SelectProductoPage : TabbedPage
+    {
+        public SelectProductoPage(ClienteModel cliente, VendedorModel vendedor)
+        {
+            InitializeComponent();
+            BindingContext = new SelectProductoViewModel(vendedor, cliente);
+
+            formaPagoPicker.SelectedIndex = 0;
+        }
+
+        private void FormaPagoPicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            string selectedFormaPago = picker.SelectedItem.ToString();
+
+            var viewModel = (SelectProductoViewModel)BindingContext;
+            viewModel.Pedido.CondPago = selectedFormaPago;
+        }
+
+        private void TipoDocPicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            string selectedTipoDoc = picker.SelectedItem.ToString();
+            var viewModel = (SelectProductoViewModel)BindingContext;
+
+            if (selectedTipoDoc == "Factura")
+            {
+                viewModel.Pedido.TipoDoc = "F";
+            }
+
+            if (selectedTipoDoc == "Boleta")
+            {
+                viewModel.Pedido.TipoDoc = "B";
+            }
+        }
+
+        private void DireccionPicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            int selectedIndex = picker.SelectedIndex;
+
+            if (selectedIndex != -1)
+            {
+                var viewModel = (SelectProductoViewModel)BindingContext;
+                var selectedDireccion = viewModel.Cliente.Direcciones[selectedIndex];
+                viewModel.Pedido.IdDirCli = selectedDireccion.Codigo;
+            }
+        }
+
+        private void FleteCheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            var checkBox = (CheckBox)sender;
+            bool isChecked = checkBox.IsChecked;
+
+            var viewModel = (SelectProductoViewModel)BindingContext;
+            viewModel.Pedido.Flete = isChecked;
+        }
+
+        private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+                return;
+
+            var viewModel = BindingContext as SelectProductoViewModel;
+            var selectedProducto = e.SelectedItem as ProductoModel;
+
+            viewModel?.SeleccionarProductoCommand.Execute(selectedProducto);
+
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
