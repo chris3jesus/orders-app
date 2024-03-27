d3d2d7663834e7058115c226625af39856998177
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-03-27 09:58:55 -0500

    feat: add select cliente page

---
+++ OrdersApp/OrdersApp/Vistas/Pedidos/SelectClientePage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using OrdersApp.Modelos;
+using OrdersApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OrdersApp.Vistas.Pedidos
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SelectClientePage : ContentPage
+    {
+        public SelectClientePage(VendedorModel vendedor)
+        {
+            InitializeComponent();
+            BindingContext = new SelectClienteViewModel(vendedor);
+        }
+
+        private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null) return;
+            var viewModel = BindingContext as SelectClienteViewModel;
+            var selectedCliente = e.SelectedItem as ClienteModel;
+            viewModel?.SeleccionarClienteCommand.Execute(selectedCliente);
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
