e8cbfff5d3e0dd4d624b2a56f94020a7f1bad7eb
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-03-28 13:21:30 -0500

    feat: add create cliente service

--- OrdersApp/OrdersApp/Servicios/ClientesService.cs
+++ OrdersApp/OrdersApp/Servicios/ClientesService.cs
@@ -38,5 +38,33 @@ namespace OrdersApp.Servicios
             }
             return null;
         }
+
+        public async Task<bool> RegistrarCliente(NuevoClienteModel nuevoCliente)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    string clienteJson = JsonConvert.SerializeObject(nuevoCliente);
+                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage response = await client.PostAsync(Url, new StringContent(clienteJson, Encoding.UTF8, "application/json"));
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("La solicitud POST no fue exitosa. Estado: " + response.StatusCode);
+                        return false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al registrar el cliente: " + ex.Message);
+                return false;
+            }
+        }
     }
 }
