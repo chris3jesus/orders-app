9512d4725ae4f322aba1a1827de2d9aff62af626
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-03-28 13:23:55 -0500

    feat add create cliente page

---
+++ OrdersApp/OrdersApp/Vistas/Clientes/CreateClientePage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using OrdersApp.Modelos;
+using OrdersApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OrdersApp.Vistas.Clientes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CreateClientePage : ContentPage
+    {
+        public CreateClientePage(VendedorModel vendedor)
+        {
+            InitializeComponent();
+            BindingContext = new CreateClienteViewModel(vendedor);
+        }
+    }
+}
