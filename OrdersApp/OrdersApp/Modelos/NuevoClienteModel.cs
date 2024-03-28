487f1d6893b802192b0c48a4a20e1a71580beafd
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-03-28 13:20:41 -0500

    feat: add new cliente model

---
+++ OrdersApp/OrdersApp/Modelos/NuevoClienteModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrdersApp.Modelos
+{
+    public class NuevoClienteModel
+    {
+        public bool Creado { get; set; } = false;
+        public int CodVen { get; set; }
+        public string NroDoc { get; set; }
+        public string Nombre { get; set; }
+        public string Comercial { get; set; }
+        public string Direccion { get; set; }
+    }
+}
