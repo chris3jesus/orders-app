d68b1d1e291210366b5c1a8ea66b75d5012f4eb3
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-04-02 10:01:35 -0500

    fix: add valor producto model

--- OrdersApp/OrdersApp/Modelos/ProductoModel.cs
+++ OrdersApp/OrdersApp/Modelos/ProductoModel.cs
@@ -10,6 +10,7 @@ namespace OrdersApp.Modelos
         public string Descripcion { get; set; }
         public string Marca { get; set; }
         public string Presentacion { get; set; }
+        public decimal Valor { get; set; }
         public decimal Precio { get; set; }
         public int Stock { get; set; }
     }
