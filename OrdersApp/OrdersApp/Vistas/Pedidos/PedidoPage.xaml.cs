3ac5ad6de086e61ef030397f7932d7688bdf6821
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-04-02 12:16:20 -0500

    feat: add pedido page

---
+++ OrdersApp/OrdersApp/Vistas/Pedidos/PedidoPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using OrdersApp.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OrdersApp.Vistas.Pedidos
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PedidoPage : ContentPage
+    {
+        public PedidoPage(PedidosModel pedidos)
+        {
+            InitializeComponent();
+            BindingContext = pedidos;
+        }
+    }
+}
