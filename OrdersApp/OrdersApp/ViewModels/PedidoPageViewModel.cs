35814da4ca85ed6efd9f3fd3df858665ccecd67c
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-04-03 13:16:23 -0500

    feat: add pedido page view model

---
+++ OrdersApp/OrdersApp/ViewModels/PedidoPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using OrdersApp.Modelos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace OrdersApp.ViewModels
+{
+    public class PedidoPageViewModel : INotifyPropertyChanged
+    {
+        private PedidosModel _pedido;
+
+        public PedidosModel Pedido
+        {
+            get { return _pedido; }
+            set { _pedido = value; OnPropertyChanged(nameof(Pedido)); }
+        }
+
+        public PedidoPageViewModel(PedidosModel pedido)
+        {
+            Pedido = pedido;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
