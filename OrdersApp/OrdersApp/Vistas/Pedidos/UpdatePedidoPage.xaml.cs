b4fc2aeaf3256e7305d27f085647455b56faddf0
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-04-03 13:15:32 -0500

    feat: add update pedido page

---
+++ OrdersApp/OrdersApp/Vistas/Pedidos/UpdatePedidoPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using OrdersApp.Modelos;
+using OrdersApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OrdersApp.Vistas.Pedidos
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UpdatePedidoPage : TabbedPage
+    {
+        public UpdatePedidoPage(PedidosModel pedido)
+        {
+            InitializeComponent();
+            BindingContext = new UpdatePedidoViewModel(pedido);
+        }
+
+        private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null) return;
+            var viewModel = BindingContext as UpdatePedidoViewModel;
+            var selectedProducto = e.SelectedItem as ProductoModel;
+            viewModel?.SeleccionarProductoCommand.Execute(selectedProducto);
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
