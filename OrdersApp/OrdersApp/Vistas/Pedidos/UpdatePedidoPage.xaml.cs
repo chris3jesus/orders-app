d8f771f47b522c45ab353a1e9febac9fed1a5cc3
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-04-06 10:14:38 -0500

    feat: add udpate header on update pedido page

--- OrdersApp/OrdersApp/Vistas/Pedidos/UpdatePedidoPage.xaml.cs
+++ OrdersApp/OrdersApp/Vistas/Pedidos/UpdatePedidoPage.xaml.cs
@@ -18,6 +18,89 @@ namespace OrdersApp.Vistas.Pedidos
         {
             InitializeComponent();
             BindingContext = new UpdatePedidoViewModel(pedido);
+
+            formaPagoPicker.SelectedIndex = 0;
+            fleteCheckBox.IsChecked = pedido.Flete;
+
+            if (pedido.TipoDoc == "F")
+            {
+                tipoDocPicker.SelectedIndex = 0;
+            }
+            if (pedido.TipoDoc == "B")
+            {
+                tipoDocPicker.SelectedIndex = 1;
+            }
+
+            SelectIndexBasedOnPedidoId();
+        }
+
+        private async void SelectIndexBasedOnPedidoId()
+        {
+            var viewModel = (UpdatePedidoViewModel)BindingContext;
+            var pedido = viewModel.Pedido;
+            int idDireccionPedido = pedido.IdDirCli;
+
+            int indiceSeleccionado = -1;
+            await Task.Delay(100);
+
+            for (int i = 0; i < viewModel.Cliente.Direcciones.Count; i++)
+            {
+                if (viewModel.Cliente.Direcciones[i].Codigo == idDireccionPedido)
+                {
+                    indiceSeleccionado = i;
+                    break;
+                }
+            }
+            direccionPicker.SelectedIndex = indiceSeleccionado;
+
+        }
+
+        private void FormaPagoPicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            string selectedFormaPago = picker.SelectedItem.ToString();
+
+            var viewModel = (UpdatePedidoViewModel)BindingContext;
+            viewModel.Pedido.CondPago = selectedFormaPago;
+        }
+
+        private void TipoDocPicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            string selectedTipoDoc = picker.SelectedItem.ToString();
+            var viewModel = (UpdatePedidoViewModel)BindingContext;
+
+            if (selectedTipoDoc == "Factura")
+            {
+                viewModel.Pedido.TipoDoc = "F";
+            }
+
+            if (selectedTipoDoc == "Boleta")
+            {
+                viewModel.Pedido.TipoDoc = "B";
+            }
+        }
+
+        private void DireccionPicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            int selectedIndex = picker.SelectedIndex;
+
+            if (selectedIndex != -1)
+            {
+                var viewModel = (UpdatePedidoViewModel)BindingContext;
+                var selectedDireccion = viewModel.Cliente.Direcciones[selectedIndex];
+                viewModel.Pedido.IdDirCli = selectedDireccion.Codigo;
+            }
+        }
+
+        private void FleteCheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            var checkBox = (CheckBox)sender;
+            bool isChecked = checkBox.IsChecked;
+
+            var viewModel = (UpdatePedidoViewModel)BindingContext;
+            viewModel.Pedido.Flete = isChecked;
         }
 
         private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
