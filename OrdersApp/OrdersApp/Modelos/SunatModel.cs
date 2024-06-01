81189633f17d4b4bc5d0a76d8aa75665369a7c9b
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-06-01 16:26:09 -0500

    feat: add sunat model

---
+++ OrdersApp/OrdersApp/Modelos/SunatModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrdersApp.Modelos
+{
+    public class SunatModel
+    {
+        public string Documento { get; set; }
+        public string Comercial { get; set; }
+        public string Estado { get; set; }
+        public string Domicilio { get; set; }
+        public DateTime FechaConsulta { get; set; }
+    }
+}
