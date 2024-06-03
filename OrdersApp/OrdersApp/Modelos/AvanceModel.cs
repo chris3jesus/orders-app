c00d5b0ba6fbbf1087c51d61300bc00f82be511d
Jesus Carrasco <cjesus.sistemas@gmail.com>
2024-06-02 21:46:37 -0500

    feat: add avance model

---
+++ OrdersApp/OrdersApp/Modelos/AvanceModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrdersApp.Modelos
+{
+    public class AvanceModel
+    {
+        public string Vendedor { get; set; }
+        public decimal Cuota { get; set; }
+        public decimal Avance { get; set; }
+        public decimal Saldo { get; set; }
+        public decimal Porcentaje { get; set; }
+        public decimal Saldo95 { get; set; }
+    }
+
+    public class AvanceVendedorModel
+    {
+        public string Vendedor { get; set; }
+        public decimal Cuota { get; set; }
+        public decimal Avance { get; set; }
+        public decimal Saldo { get; set; }
+        public decimal Porcentaje { get; set; }
+    }
+
+    public class AvanceLineasModel
+    {
+        public string Linea { get; set; }
+        public decimal Cuota { get; set; }
+        public decimal Avance { get; set; }
+        public decimal Saldo { get; set; }
+        public decimal Porcentaje { get; set; }
+        public decimal Saldo95 { get; set; }
+    }
+
+    public class AvanceClientesModel
+    {
+        public string Linea { get; set; }
+        public decimal Cuota { get; set; }
+        public decimal Avance { get; set; }
+        public decimal Porcentaje { get; set; }
+    }
+
+    public class MessageModel
+    {
+        public string Message { get; set; }
+    }
+
+    public class CreditModel
+    {
+        public string ScrumMaster { get; set; }
+        public string ProductOwner { get; set; }
+    }
+}
